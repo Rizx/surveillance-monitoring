1a31813d34c4fd971d3ad4c367c7fbf0fda6cfa2
Rizx <noprazain@gmail.com>
2023-01-23 05:20:00 +0700

    remove options

--- api/Startup.cs
+++ api/Startup.cs
@@ -42,8 +42,6 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            Options.FromFile();
-
             services.RegisterAppSettings(Configuration);
 
             services.AddSingleton<GlobalExceptionHandler>();
