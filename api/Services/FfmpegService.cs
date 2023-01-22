8bd2d7466897ff85e88efdb31e2e9e8092411b68
Rizx <noprazain@gmail.com>
2023-01-23 05:21:38 +0700

    fixing

--- api/Services/FfmpegService.cs
+++ api/Services/FfmpegService.cs
@@ -12,43 +12,30 @@ namespace API.Services
 {
     public static class FfmpegService
     {
-        private static Process Execute(string command)
-        {
-            try
-            {
-                return Process.Start("cmd.exe", $"/k {command}");
-            }
-            catch
-            {
-                return null;
-            }
-        }
-
-        // private static Dictionary<string, Process> processActive = new Dictionary<string, Process>();
+        private static readonly Dictionary<string, int> processActive = new ();
 
         public static bool Start(string camera, string parameters, string urlSource, string filePath)
         {
             try
             {
-                // var command = $"ffmpeg -rtsp_transport tcp -i {camera.VideoUrl} -an -c:v libx264 -crf 21 -preset veryfast -fflags nobuffer -flags low_delay -t 20 -f hls -hls_time 1 -hls_list_size 3 -hls_flags delete_segments {filename}";
                 var command = string.Format(parameters, urlSource, filePath);
-                if(Options.Instance.Manager == null)
-                    Options.Instance.Manager = new();
-
-                if (Options.Instance.Manager.TryGetValue(camera, out int processid))
+                if (processActive.TryGetValue(camera, out int processid))
                 {
-                    var exist = Process.GetProcessById(processid);
-                    if (exist != null)
+                    try
                     {
-                        exist.Kill();
-                        Thread.Sleep(500);
+                        var exist = Process.GetProcessById(processid);
+                        if (exist != null)
+                        {
+                            exist.Kill();
+                            Thread.Sleep(500);
+                        }
                     }
-                    Options.Instance.Manager.Remove(camera);
+                    catch { }
+                    processActive.Remove(camera);
                 }
 
-                var process = Execute(command);
-                Options.Instance.Manager.Add(camera, process.Id);
-                Options.ToFile();
+                var process = Process.Start("ffmpeg.exe", command);
+                processActive.Add(camera, process.Id);
                 return true;
             }
             catch
@@ -56,25 +43,5 @@ namespace API.Services
                 return false;
             }
         }
-
-        // public static bool Stop(string camera)
-        // {
-        //     try
-        //     {
-        //         if (!processActive.ContainsKey(camera))
-        //             return true;
-
-        //         var process = processActive[camera];
-        //         process.Kill();
-        //         process.Close();
-        //         process.Dispose();
-        //         processActive.Remove(camera);
-        //         return true;
-        //     }
-        //     catch
-        //     {
-        //         return false;
-        //     }
-        // }
     }
 }
